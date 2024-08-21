d3ecbce16f63daf47aa32f2e93c78a8714136cc9
Jay Prakash <jprakash@csod.com>
2024-08-22 02:12:45 +0530

    fibonacci series script

---
+++ fibonacci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#take value from user for number of terms
+
+echo -n "Enter number of tenms: "
+read n
+
+#initialise the variable
+
+a=0
+b=1
+
+#Print first two numbers in series
+
+echo -n "$a $b "
+
+#for fibonacci series
+
+for ((i=2; i<n; i++))
+do
+        c=$((a + b))
+        echo -n "$c "
+        a=$b
+        b=$c
+
+done
+
+echo
